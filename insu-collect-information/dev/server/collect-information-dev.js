3acd240f5ddf9a2958786cd63739a28086702beb
rlarlejrwl56 <rlarlejr3178@naver.com>
2024-01-24 11:11:33 +0900

    사고접수, 보험청구서 추가

--- insu-collect-information/dev/server/collect-information-dev.js
+++ insu-collect-information/dev/server/collect-information-dev.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+/*var createError = require('http-errors');*/
 var express = require('express');
 var path = require('path');
 // var cookieParser = require('cookie-parser');
@@ -33,10 +33,17 @@ app.use(bodyParser({limit: '50mb'}));  // pdf body 용량문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../client'))); // service
+app.use(express.static(path.join(__dirname, '../client/build'))); // service
 
 
-
+app.get("/", (req, res) => {
+  res.set({
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Date: Date.now()
+  });
+  res.sendFile(path.join(__dirname, "build", "/index.html"));
+});
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
@@ -67,6 +74,10 @@ app.use(allowCORS); // localhost 에서 개발할 때 이걸 열어주지 않으
 app.use('/api/v1', api1001);
 app.use('/api/v1', api1001_BATCH);
 app.use('/', router);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 var port = 20102
 app.listen(port, function() {
